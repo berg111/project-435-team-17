7d59ea7f2f5e2034ca55b830abb0d2fd29ffa363
Jace Thomas <jacethomas28@tamu.edu>
2023-11-07 12:29:42 -0600

    added threads

--- OMP/odd-even.cpp
+++ OMP/odd-even.cpp
@@ -13,7 +13,7 @@ void oddEvenSort(std::vector<int> &array, int threads){
     int size = array.size();
     int i = 0;
     int phase = 0;
-    #pragma omp parallel num_threads(threads) default(none) shared(array,size) private(i,phase)
+    #pragma omp parallel shared(array,size)
     for (size_t phase = 0; phase < size; phase++){
 
 
@@ -43,11 +43,29 @@ void oddEvenSort(std::vector<int> &array, int threads){
     }
 }
 
+void generate_array(std::vector<int>& array, int size)
+{
+    array.resize(size);
+
+	for(int i = 0; i < size; i++){
+		array[i] = rand() % 100;
+    }
+}
+
 int main(int argc, char** argv) {
 
-    std::vector<int> array = {10, 9, 73, 8, 12, 5};
+        
 
     int threads = atoi(argv[1]);
+    int arraySize = atoi(argv[2]);
+
+    omp_set_num_threads(threads);
+
+    std::cout << "threads";
+
+    std::vector<int> array;
+
+    generate_array(array, arraySize);
 
     oddEvenSort(array, threads);
 
@@ -56,6 +74,7 @@ int main(int argc, char** argv) {
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
+
 
 
 }
