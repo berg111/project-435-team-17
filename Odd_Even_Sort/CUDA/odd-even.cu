8926d28431cc8d6545fa9cd9ead10f545427b9f9
Jace Thomas <jacethomas28@tamu.edu>
2023-12-03 15:51:54 -0600

    CUDA update

--- Odd_Even_Sort/CUDA/odd-even.cu
+++ Odd_Even_Sort/CUDA/odd-even.cu
@@ -15,6 +15,16 @@
 int NUM_VALUES;
 float timeA, timeB, timeC, bandwidth = 0.0;
 
+const char* main = "main";
+const char* data_init = "data_init";
+const char* correctness_check = "correctness_check";
+const char* comm = "comm";
+const char* comm_small = "comm_small";
+const char* comm_large = "comm_large";
+const char* comp = "comp";
+const char* comp_small = "comp_small";
+const char* comp_large = "comp_large";
+
 __global__ void oddEvenSortStep(int* array, int size, int phase){
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -44,11 +54,11 @@ void print_elapsed(clock_t start, clock_t stop)
   printf("Elapsed time: %.3fs\n", elapsed);
 }
 
-void print_average(clock_t start, clock_t stop, int threads)
-{
-  double elapsed = (((double) (stop - start)) / CLOCKS_PER_SEC) / threads;
-  printf("Average time: %.3fs\n", elapsed);
-}
+// void print_average(clock_t start, clock_t stop, int threads)
+// {
+//   double elapsed = (((double) (stop - start)) / CLOCKS_PER_SEC) / threads;
+//   printf("Average time: %.3fs\n", elapsed);
+// }
 
 
 
@@ -101,7 +111,17 @@ int main(int argc, char** argv){
     int threads = atoi(argv[1]);
     int arraySize = atoi(argv[2]);
     int input = atoi(argv[3]);
-
+    int blocks = arraySize / threads;
+    std::string inputType;
+    if(input == 1){
+        inputType = "Random";
+    }
+    else if(input == 2){
+        inputType = "Sorted";
+    }
+    else if(input == 3){
+        inputType = "Reverse Sorted";
+    }
     cali::ConfigManager mgr;
     mgr.start();
 
@@ -109,8 +129,10 @@ int main(int argc, char** argv){
     dim3 threadsPerBlock(threads, 1, 1);
 
     std::vector<int> array;
-
+    CALI_MARk_BEGIN(main);
+    CALI_MARK_BEGIN(data_init)
     generate_array(array, arraySize, input);
+    
     std::cout << arraySize << "Threads: " << threads << "Input type: " << input << std::endl;
 
     // std::cout << "UNSorted Array: ";
@@ -121,10 +143,16 @@ int main(int argc, char** argv){
     int* gpu_array;
     size_t size = array.size() * sizeof(int);
     cudaMalloc((void**) &gpu_array, size);
+    CALI_MARK_END(data_init);
 
+    CALI_MARK_BEGIN(comm)
+    CALI_MARK_BEGIN(comm_large)
     cudaMemcpy(gpu_array, array.data(), size, cudaMemcpyHostToDevice);
+    CALI_MARK_END(comm_large)
+    CALI_MARK_END(comm);
 
-    CALI_MARK_BEGIN("comp");
+    CALI_MARK_BEGIN(comp);
+    CALI_MARK_BEGIN(comp);
     cudaEventCreate(&sort_step_start);
     cudaEventCreate(&sort_step_end);
     cudaEventRecord(sort_step_start);
@@ -146,15 +174,18 @@ int main(int argc, char** argv){
     cudaEventRecord(sort_step_end);
     cudaEventSynchronize(sort_step_end);
     cudaEventElapsedTime(&timeB, sort_step_start, sort_step_end);
+    CALI_MARK_END(comp_large);
+    CALI_MARK_END(comp);
 
-    CALI_MARK_END("comp");
-
+    CALI_MARK_BEGIN(comm);
+    CALI_MARK_BEGIN(comm_large);
     cudaMemcpy(array.data(), gpu_array, size, cudaMemcpyDeviceToHost);
-
+    CALI_MARK_END(comm_large);
+    CALI_MARK_END(comm);
     cudaFree(gpu_array);
 
     // printf("Time elapsed: %f ", timeB);
-
+    CALI_MARK_BEGIN(correctness_check);
     if (isSorted(array)) {
         // std::cout << "Sorted Array: ";
         // for (int i = 0; i < array.size(); ++i) {
@@ -169,16 +200,25 @@ int main(int argc, char** argv){
         // }
         std::cout << std::endl;
     }
+    CALI_MARK_END(correctness_check);
+    CALI_MARK_END(main)
 
     adiak::init(NULL);
-    adiak::user();
-    adiak::launchdate();
-    adiak::libraries();
-    adiak::cmdline();
-    adiak::clustername();
-    adiak::value("num_threads", threads);
-    adiak::value("num_vals", arraySize);
-    adiak::value("Sort_time", timeB);
+    adiak::launchdate();    // launch date of the job
+    adiak::libraries();     // Libraries used
+    adiak::cmdline();       // Command line used to launch the job
+    adiak::clustername();   // Name of the cluster
+    adiak::value("Algorithm", "Odd-Even Sort"); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+    adiak::value("ProgrammingModel", "CUDA"); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+    adiak::value("Datatype", "Int"); // The datatype of input elements (e.g., double, int, float)
+    adiak::value("SizeOfDatatype", sizeOf(int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+    adiak::value("InputSize", arraySize); // The number of elements in input dataset (1000)
+    adiak::value("InputType", inputType); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+    //adiak::value("num_procs", num_procs); // The number of processors (MPI ranks)
+    adiak::value("num_threads", threads); // The number of CUDA or OpenMP threads
+    adiak::value("num_blocks", blocks); // The number of CUDA blocks 
+    adiak::value("group_num", 17); // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "Online/AI/Handwritten") // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
 
     mgr.stop();
     mgr.flush();
--- Odd_Even_Sort/MPI/odd-even-mpi.cpp
+++ Odd_Even_Sort/MPI/odd-even-mpi.cpp
@@ -79,13 +79,6 @@ int main(int argc, char** argv) {
     int array[size];
     MPI_Status status;
 
-    double worker_receive_time,       /* Buffer for worker recieve times */
-    worker_calculation_time,      /* Buffer for worker calculation times */
-    worker_send_time = 0;         /* Buffer for worker send times */
-    double whole_computation_time,    /* Buffer for whole computation time */
-    master_initialization_time,   /* Buffer for master initialization time */
-    master_send_receive_time = 0; /* Buffer for master send and receive time */
-
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
@@ -99,130 +92,12 @@ int main(int argc, char** argv) {
     numworkers = numtasks - 1;
 
     MPI_Comm new_comm;
-    if(taskid > 0){
-        MPI_Comm_split(MPI_COMM_WORLD, 1, taskid, &new_comm);
-    }
-    else if(taskid == 0){
-        MPI_Comm_split(MPI_COMM_WORLD, MPI_UNDEFINED, taskid, &new_comm);
-    }   
+    MPI_Comm_split(MPI_COMM_WORLD, task_id > 0, task_id - 1, &newcomm);
 
     double whole_start_time = MPI_Wtime();
     double recv_total_time, send_total_time, calc_total_time = 0;
 
-    if (taskid == MASTER) {
 
-        generate_array(array, size, arrayINput);
-
-        // printf("Unsorted Array: ");
-        // for (int i = 0; i < size; i++) {
-        //     printf("%d ", array[i]);
-        // }
-        mtype = FROM_MASTER;
-        for (dest = 1; dest <= numworkers; dest++) {
-            MPI_Send(&size, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            MPI_Send(array, size, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-}
-
-        CALI_MARK_BEGIN("comp");
-        oddEvenSort(array, size);
-                
-        
-        mtype = FROM_WORKER;
-        for (int i = 1; i <= numworkers; i++) {
-        source = i;
-        MPI_Recv(array, size, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-        }
-
-        CALI_MARK_END("comp");
-
-        
-        // printf("Sorted Array: ");
-        // for (int i = 0; i < size; i++) {
-        // printf("%d ", array[i]);
-        // }
-        printf("\n");
-    } if(taskid > MASTER) {
-
-        mtype = FROM_MASTER;
-        MPI_Recv(&size, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(array, size, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-
-        double calc_start_time = MPI_Wtime();
-        oddEvenSort(array, size);
-        double calc_end_time = MPI_Wtime();
-        calc_total_time = calc_end_time - calc_start_time;
-        mtype = FROM_WORKER;
-        MPI_Send(array, size, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-    }
-
-    double whole_end_time = MPI_Wtime();
-    whole_computation_time = whole_end_time - whole_start_time;
-
-       double worker_receive_time_max,
-      worker_receive_time_min,
-      worker_receive_time_sum,
-      worker_recieve_time_average,
-      worker_calculation_time_max,
-      worker_calculation_time_min,
-      worker_calculation_time_sum,
-      worker_calculation_time_average,
-      worker_send_time_max,
-      worker_send_time_min,
-      worker_send_time_sum,
-      worker_send_time_average = 0; // Worker statistic values.
-
-      if(new_comm != MPI_COMM_NULL){
-
-        MPI_Reduce(&calc_total_time, &worker_calculation_time_min, 1, MPI_DOUBLE, MPI_MIN, 0, new_comm);
-        MPI_Reduce(&calc_total_time, &worker_calculation_time_max, 1, MPI_DOUBLE, MPI_MAX, 0, new_comm);
-        MPI_Reduce(&calc_total_time, &worker_calculation_time_sum, 1, MPI_DOUBLE, MPI_SUM, 0, new_comm);
-        worker_calculation_time_average = worker_calculation_time_sum / numworkers;
-      }
-
-
-       if (taskid == 0)
-   {
-      // Master Times
-      printf("******************************************************\n");
-      printf("Master Times:\n");
-      printf("Whole Computation Time: %f \n", whole_computation_time);
-      printf("\n******************************************************\n");
-
-      // Add values to Adiak
-      adiak::value("MPI_Reduce-whole_computation_time", whole_computation_time);
-
-      // Must move values to master for adiak
-      mtype = FROM_WORKER;
-
-      MPI_Recv(&worker_calculation_time_max, 1, MPI_DOUBLE, 1, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&worker_calculation_time_min, 1, MPI_DOUBLE, 1, mtype, MPI_COMM_WORLD, &status);
-      MPI_Recv(&worker_calculation_time_average, 1, MPI_DOUBLE, 1, mtype, MPI_COMM_WORLD, &status);
-
-
-      adiak::value("MPI_Reduce-worker_calculation_time_max", worker_calculation_time_max);
-      adiak::value("MPI_Reduce-worker_calculation_time_min", worker_calculation_time_min);
-      adiak::value("MPI_Reduce-worker_calculation_time_average", worker_calculation_time_average);
-   }
-   else if (taskid == 1)
-   { // Print only from the first worker.
-      // Print out worker time results.
-      
-      // Compute averages after MPI_Reduce
-      worker_calculation_time_average = worker_calculation_time_sum / (double)numworkers;
-
-
-      printf("******************************************************\n");
-      printf("Worker Times:\n");
-      printf("Worker Calculation Time Max: %f \n", worker_calculation_time_max);
-      printf("Worker Calculation Time Min: %f \n", worker_calculation_time_min);
-      printf("Worker Calculation Time Average: %f \n", worker_calculation_time_average);
-      printf("\n******************************************************\n");
-
-      mtype = FROM_WORKER;
-      MPI_Send(&worker_calculation_time_max, 1, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-      MPI_Send(&worker_calculation_time_min, 1, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-      MPI_Send(&worker_calculation_time_average, 1, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-   }
 
     MPI_Finalize();
     return 0;
